e5408cf1f63d2f22cb35cf5a8d7806c1675b0a57
lolmaus (Andrey Mikhaylov) <lolmaus@gmail.com>
2017-09-07 16:13:13 +0300

    Enable toggling with a swipe gesture, fixes #49

--- addon/components/x-toggle-switch/component.js
+++ addon/components/x-toggle-switch/component.js
@@ -1,12 +1,23 @@
 import Component from '@ember/component';
 import { computed } from "@ember/object"
 import layout from './template';
+import RecognizerMixin from 'ember-gestures/mixins/recognizers';
 
-export default Component.extend({
+export default Component.extend(RecognizerMixin, {
   layout,
   tagName: 'span',
   classNames: ['x-toggle-container'],
   classNameBindings: ['size', 'disabled:x-toggle-container-disabled', 'value:x-toggle-container-checked'],
+
+  recognizers: 'swipe',
+
+  swipeRight() {
+    this.get('sendToggle')(true);
+  },
+
+  swipeLeft() {
+    this.get('sendToggle')(false);
+  },
 
   themeClass: computed('theme', function() {
     return `x-toggle-${this.get('theme') || 'default'}`;
